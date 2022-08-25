d3be50ade33a1d3daab4f713370b7d2208b428bc
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-08-25 18:58:05 -0300

    Criando o service de medico

---
+++ src/medicos/medicos.service.ts
@@ -0,0 +1,67 @@
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { CreateMedicoDTO } from './dto/create-medico.dto';
+import { Medico } from './entities/medico.entity';
+
+@Injectable()
+export class MedicosService {
+  constructor(
+    @Inject('MedicoRepository') private medicoRepository: Repository<Medico>,
+  ) {}
+
+  async create(createMedicoDto: CreateMedicoDTO) {
+    const existeMedico = await this.medicoRepository.findOne({
+      where: { crm: createMedicoDto.crm },
+    });
+
+    if (existeMedico) {
+      throw new BadRequestException('O CRM informado já está cadastrado');
+    }
+
+    const medico = this.medicoRepository.create({ ...createMedicoDto });
+
+    return this.medicoRepository.save(medico);
+  }
+
+  async findAll() {
+    return this.medicoRepository.find();
+  }
+
+  async findOne(id: string) {
+    const medico = await this.medicoRepository.findOne({ where: { id } });
+
+    if (!medico) {
+      throw new NotFoundException(
+        `Não foi encontrado um médico com o ID : ${id}`,
+      );
+    }
+
+    return medico;
+  }
+
+  async update(id: string, updateMedicoDto: Partial<CreateMedicoDTO>) {
+    const medico = await this.medicoRepository.preload({
+      id,
+      ...updateMedicoDto,
+    });
+
+    if (!medico) {
+      throw new NotFoundException(
+        `Não foi encontrado um médico com o ID : ${id}`,
+      );
+    }
+
+    return this.medicoRepository.save(medico);
+  }
+
+  async remove(id: string) {
+    const medico = await this.findOne(id);
+
+    return this.medicoRepository.remove(medico);
+  }
+}
