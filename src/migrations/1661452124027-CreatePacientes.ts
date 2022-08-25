40cc10e4f0023d99e45c1af9951e9389d2100fa6
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-08-25 15:32:25 -0300

    Criando a tabela de pacientes

---
+++ src/migrations/1661452124027-CreatePacientes.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePacientes1661452124027 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'pacientes',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'data_nascimento',
+            type: 'timestamp',
+          },
+          {
+            name: 'telefone',
+            type: 'varchar',
+            length: '15',
+          },
+          {
+            name: 'convenio',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('pacientes');
+  }
+}
