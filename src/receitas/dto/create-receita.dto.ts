89cea789bcca1718b584826f01d5786fb8d76970
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-08-26 09:59:17 -0300

    Criando o service de receitas

---
+++ src/receitas/dto/create-receita.dto.ts
@@ -0,0 +1,3 @@
+export interface CreateReceitaDTO {
+  descricao: string;
+}
---
+++ src/receitas/receitas.service.ts
@@ -0,0 +1,54 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { CreateReceitaDTO } from './dto/create-receita.dto';
+import { Receita } from './entities/receita.entity';
+
+@Injectable()
+export class ReceitasService {
+  constructor(
+    @Inject('ReceitaRepository') private receitaRepository: Repository<Receita>,
+  ) {}
+
+  async create(createReceitaDto: CreateReceitaDTO) {
+    const receita = this.receitaRepository.create({ ...createReceitaDto });
+
+    return this.receitaRepository.save(receita);
+  }
+
+  async findAll() {
+    return this.receitaRepository.find();
+  }
+
+  async findOne(id: string) {
+    const receita = await this.receitaRepository.findOne({ where: { id } });
+
+    if (!receita) {
+      throw new NotFoundException(
+        `Não foi encontrado uma receita com o ID : ${id}`,
+      );
+    }
+
+    return receita;
+  }
+
+  async update(id: string, updateReceitaDto: Partial<CreateReceitaDTO>) {
+    const receita = await this.receitaRepository.preload({
+      id,
+      ...updateReceitaDto,
+    });
+
+    if (!receita) {
+      throw new NotFoundException(
+        `Não foi encontrado um médico com o ID : ${id}`,
+      );
+    }
+
+    return this.receitaRepository.save(receita);
+  }
+
+  async remove(id: string) {
+    const receita = await this.findOne(id);
+
+    return this.receitaRepository.remove(receita);
+  }
+}
