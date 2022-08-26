7060f4092d98906a9d56910f73ddb907503e1efe
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-08-26 10:35:19 -0300

    Criando as pastas modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConsultasController } from './consultas/consultas.controller';
-import { PacientesController } from './pacientes/pacientes.controller';
-import { ReceitasController } from './receitas/receitas.controller';
-import { MedicosController } from './medicos/medicos.controller';
-import { MedicosService } from './medicos/medicos.service';
-import { ConsultasService } from './consultas/consultas.service';
-import { PacientesService } from './pacientes/pacientes.service';
-import { ReceitasService } from './receitas/receitas.service';
+
+import { ConsultasModule } from './consultas/consultas.module';
+import { MedicosModule } from './medicos/medicos.module';
+import { PacientesModule } from './pacientes/pacientes.module';
+import { ReceitasModule } from './receitas/receitas.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    MedicosController,
-    ConsultasController,
-    PacientesController,
-    ReceitasController,
-  ],
-  providers: [
-    MedicosService,
-    ConsultasService,
-    PacientesService,
-    ReceitasService,
-  ],
+  imports: [ConsultasModule, MedicosModule, PacientesModule, ReceitasModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
