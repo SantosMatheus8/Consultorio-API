299516fb519f3e39b33bbc3c3837519ac907f0dc
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-08-26 09:07:12 -0300

    Criando o controller de pacientes

---
+++ src/pacientes/pacientes.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreatePacienteDTO } from './dto/create-paciente.dto';
+import { PacientesService } from './pacientes.service';
+
+@Controller('pacientes')
+export class PacientesController {
+  constructor(private readonly pacientesService: PacientesService) {}
+
+  @Post()
+  create(@Body() createPacienteDto: CreatePacienteDTO) {
+    return this.pacientesService.create(createPacienteDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.pacientesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.pacientesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() createPacienteDto: CreatePacienteDTO,
+  ) {
+    return this.pacientesService.update(id, createPacienteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.pacientesService.remove(id);
+  }
+}
