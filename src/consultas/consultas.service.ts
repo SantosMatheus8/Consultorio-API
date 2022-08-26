058a3876e9b2ef5c3cd5f734e606d078ca5d7b3d
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-08-26 08:37:22 -0300

    Criando o service de consultas

---
+++ src/consultas/consultas.service.ts
@@ -0,0 +1,90 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Medico } from 'src/medicos/entities/medico.entity';
+import { Paciente } from 'src/pacientes/entities/paciente.entity';
+import { Receita } from 'src/receitas/entities/receita.entity';
+import { Repository } from 'typeorm';
+import { CreateConsultaDTO } from './dto/create-consulta.dto';
+import { Consulta } from './entities/consulta.entity';
+
+@Injectable()
+export class ConsultasService {
+  constructor(
+    @Inject('ConsultaRepository')
+    private consultaRepository: Repository<Consulta>,
+    @Inject('MedicoRepository') private medicoRepository: Repository<Medico>,
+    @Inject('ReceitaRepository') private receitaRepository: Repository<Receita>,
+    @Inject('PacienteRepository')
+    private pacienteRepository: Repository<Paciente>,
+  ) {}
+
+  async create(createConsultaDto: CreateConsultaDTO) {
+    const existeMedico = await this.medicoRepository.findOne({
+      where: { id: createConsultaDto.medicoId },
+    });
+    const existeReceita = await this.receitaRepository.findOne({
+      where: { id: createConsultaDto.receitaId },
+    });
+    const existePaciente = await this.pacienteRepository.findOne({
+      where: { id: createConsultaDto.pacienteId },
+    });
+
+    if (!existeMedico) {
+      throw new NotFoundException(
+        `O médico de ID : ${createConsultaDto.medicoId}, não foi encontrado`,
+      );
+    }
+
+    if (!existeReceita) {
+      throw new NotFoundException(
+        `A receita de ID : ${createConsultaDto.receitaId}, não foi encontrado`,
+      );
+    }
+
+    if (!existePaciente) {
+      throw new NotFoundException(
+        `O paciente de ID : ${createConsultaDto.pacienteId}, não foi encontrado`,
+      );
+    }
+
+    const consulta = this.consultaRepository.create({ ...createConsultaDto });
+
+    return this.consultaRepository.save(consulta);
+  }
+
+  async findAll() {
+    return this.consultaRepository.find();
+  }
+
+  async findOne(id: string) {
+    const consulta = await this.consultaRepository.findOne({ where: { id } });
+
+    if (!consulta) {
+      throw new NotFoundException(
+        `Não foi encontrado uma consulta com o ID : ${id}`,
+      );
+    }
+
+    return consulta;
+  }
+
+  async upload(id: string, updateConsultaDto: Partial<CreateConsultaDTO>) {
+    const consulta = await this.consultaRepository.preload({
+      id,
+      ...updateConsultaDto,
+    });
+
+    if (!consulta) {
+      throw new NotFoundException(
+        `Não foi encontrado uma consulta com o ID : ${id}`,
+      );
+    }
+
+    return this.consultaRepository.save(consulta);
+  }
+
+  async remove(id: string) {
+    const consulta = await this.findOne(id);
+
+    return this.consultaRepository.remove(consulta);
+  }
+}
---
+++ src/consultas/dto/create-consulta.dto.ts
@@ -0,0 +1,6 @@
+export interface CreateConsultaDTO {
+  dataConsulta: Date;
+  medicoId: string;
+  receitaId: string;
+  pacienteId: string;
+}
